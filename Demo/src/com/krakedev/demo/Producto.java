278155deee032825b5dfac74312269923b9d5632
Mateo Molina <robert.mateo.3012@gmail.com>
2025-11-23 18:18:40 -0500

    se realizo el reto 14

---
+++ Demo/src/com/krakedev/demo/Producto.java
@@ -0,0 +1,49 @@
+package com.krakedev.demo;
+
+public class Producto {
+	private int codigo;
+	private String nombre;
+	private String descripcion;
+	private double peso;
+	
+	public Producto(int codigo,String nombre,String descripcion, double peso) {
+				this.codigo=codigo;
+				this.nombre=nombre;
+				this.descripcion=descripcion;
+				this.peso=peso;
+				
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	
+	public void imprimir(){
+		System.out.println("El codigo es : " + getCodigo());
+		System.out.println("El nombre es: " + getNombre());
+		System.out.println("La descripcion: " + getDescripcion());
+		System.out.println("El peso es: "+ getPeso());
+	}
+	
+}
